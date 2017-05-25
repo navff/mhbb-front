55fbb881991ec4670361f53024e1c0a06600089d
Burdasov Oleg <fuego0830@gmail.com>
2017-05-25 18:06:03 +0300

    ended main page search

--- src/app/shared/activity.service.ts
+++ src/app/shared/activity.service.ts
@@ -5,16 +5,22 @@ import { Http } from '@angular/http';
 export class ActivityService {
     constructor(private http: Http) {}
     searchActivities(
-
         word?: any,
         age?: any,
+        interestId?: any,
+        cityId?: any,
+        sobriety?: any,
         free?: any) {
-        let w, a, f, q;
+        let w, a, i, c, s, f, q;
 
         w = word     ? `word=${word}` : '';
         a = age      ? `age=${age}` : '';
+        i = interestId      ? `interestId=${interestId}` : '';
+        c = cityId      ? `cityId=${cityId}` : '';
+        s = sobriety      ? `sobriety=${sobriety}` : '';
         f = free     ? `free=${free}` : '';
-        q = [w, a, f].filter(function(x) { return x !== '' }).join('&');
+
+        q = [w, i, c, s, a, f].filter(function(x) { return x !== ''; }).join('&');
         return this.http.get(`http://test.mhbb.ru/b/api/activity/search?${q}`);
     }
     getAllUncheckedActivities() {
