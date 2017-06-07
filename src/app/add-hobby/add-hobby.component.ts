5133e163a9121b088f45b9087a91b93525d86329
Burdasov Oleg <fuego0830@gmail.com>
2017-06-07 15:04:39 +0300

    input image , encoding, preview

--- src/app/add-hobby/add-hobby.component.ts
+++ src/app/add-hobby/add-hobby.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
+// import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
+export class TempFilePutBody {
+  FormId: string;
+  Filename: string;
+  Data: any;
+  IsMain: boolean;
+  constructor (formid: string, name: string, data: any, ismain: boolean) {
+    this.FormId = formid;
+    this.Filename = name;
+    this.Data = data;
+    this.IsMain = ismain;
+}}
 
 @Component({
   selector: 'my-add-hobby',
@@ -9,24 +20,38 @@ import 'rxjs/add/operator/toPromise';
   styleUrls: ['./add-hobby.component.sass']
 })
 export class AddHobbyComponent {
+  formId: string;
+  isMain: true;
+  fileName: string;
+  fileData: any;
 
-  constructor(private http: Http) {}
+  constructor() {}
 
+  encodeImage(file) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+    this.fileData = reader.result;
+    console.log(this.fileData);
+    };
+  }
   fileChange(event) {
     let fileList = event.target.files;
 
     if (fileList.length > 0) {
       let file: File = fileList[0];
-      let formData: FormData = new FormData();
-      formData.append('uploadFile', file, file.name);
-      let headers = new Headers();
-      headers.append('Content-Type', 'multipart/form-data');
-      headers.append('Accept', 'application/json');
-      let options = new RequestOptions({ headers: headers });
-      this.http.post(`somwhere`, formData, options)
-      .map((response) => response.json())
-      .toPromise()
-      .then(result =>  console.log(result));
+      this.fileName = file.name;
+      this.encodeImage(file);
+
+      // let headers = new Headers();
+      // headers.append('Content-Type', 'multipart/form-data');
+      // headers.append('Accept', 'application/json');
+      // headers.append('Authorization', 'Token ABRAKADABRA');
+      // let options = new RequestOptions({ headers: headers });
+      // let body = new TempFilePutBody(this.formId, this.fileName, file, this.isMain);
+      // this.http.post(`http://test.mhbb.ru/b/api/tempfile`, body, options)
+      // .map((response) => response.json())
+      // .toPromise()
+      // .then(result =>  console.log(result));
   }
-}
-}
+}}
